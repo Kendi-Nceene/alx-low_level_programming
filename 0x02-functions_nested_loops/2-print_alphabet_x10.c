40d72bc1dba4c4630b48db1d009533c9a3c92e58
Kendi-Nceene <kendinceene@gmail.com>
2022-07-06 13:53:59 -0700

    Task 2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*prints 10 times the alphabet -> in lowercase
+*/
+void print_alphabet_x10(void)
+{
+int h;
+int x;
+for (x = 0; x < 10 ; x++)
+{
+for (h = 'a'; h <= 'z'; h++)
+{
+_putchar(h);
+}
+_putchar('\n');
+}
+}
