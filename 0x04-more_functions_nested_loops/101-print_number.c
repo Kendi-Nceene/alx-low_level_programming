263237bca97e0f7f2f0fa72aa98b85ab57720857
Kendi-Nceene <kendinceene@gmail.com>
2022-07-09 02:01:04 -0700

    Task 1. Like, comment, subscribe

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,14 +3,13 @@
 /**
  * print_number - prints an integer n
  *@n: integer to be printed
- *@void: nothing
  *
  * Return: no return
  */
 void print_number(int n)
-{	
+{
 int d = 1, i = 0, ii = 0;
-int _putchar();
+int _putchar(void);
 
 if (n < 0)
 {
@@ -26,7 +25,7 @@ d = d / 10;
 {
 while (ii < i)
 _putchar('0' + n / d);
-n = (n - (n / d) * d);
+n = (n - (n / d) d);
 d = d / 10;
 ii++;
 }
