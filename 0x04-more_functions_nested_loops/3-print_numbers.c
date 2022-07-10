21a2d3284a4cf94b4f934d4b81357d5118d8c3b1
Kendi-Nceene <kendinceene@gmail.com>
2022-07-10 07:23:02 -0700

    Task 3. The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,14 +8,12 @@
 void print_numbers(void)
 {
 int i;
-int _putchar();
+
 i = 0;
 
-while (i < 10)
-
+for (i = 0; i < 10; i++)
 {
-_putchar (i + '0');
-i++;
+_putchar(i + '0');
 }
 _putchar('\n');
 }
