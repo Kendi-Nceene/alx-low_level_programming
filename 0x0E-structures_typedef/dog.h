175fbc9fc56d5345004792bf2a47cdf7cbb6e7bb
Kendi-Nceene <kendinceene@gmail.com>
2022-08-01 14:15:23 -0700

    Task 0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,10 +3,8 @@
 
 /**
  * dog_t - Typedef for struct dog
- * struct dog - struct that stores some information of a dog
  */
-typedef struct dog
-dog_t;
+typedef struct dog dog_t;
 
 /**
  * struct dog - struct that stores some information of a dog
@@ -16,7 +14,9 @@ dog_t;
  *
  * Description: struct called "dog" that stores its name, its age
  * and the name of its owner.
+ *
  */
+
 struct dog
 {
 char *name;
@@ -24,7 +24,5 @@ float age;
 char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+
 #endif
