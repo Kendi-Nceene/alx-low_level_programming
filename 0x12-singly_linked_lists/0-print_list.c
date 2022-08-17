79f7ced599cb45c68e64039c90d8e0cca544b12d
Kendi-Nceene <kendinceene@gmail.com>
2022-08-17 07:26:45 -0700

    All done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * print_list - prints all the elements of a list_t list.
  * @h: singly linked list.
@@ -14,7 +15,7 @@ while (h != NULL)
 if (h->str == NULL)
 printf("[%d] %s\n", 0, "(nil)");
 else
-printf("[%d] %s\n", h->len, h->str);
+printf("[%d] %s\n", h->len, h->str)
 h = h->next;
 nelem++;
 }
